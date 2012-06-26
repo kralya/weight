2375f341c8cb3a6e21df9ec96eb18f643e7bb216
Alex <alexander.kuzmin@quotezzz.com>
2012-06-26 09:49:19 +0300

    Update DB name, add WELCOME_PAGE and DEPTH_SHOW constants

--- config.php
+++ config.php
@@ -2,11 +2,15 @@
 
 $username = 'root';
 $password = '';
-$db = 'time_db';
+$db = 'weight';
 $server = 'localhost';
 
 mysql_connect($server, $username, $password);
 mysql_select_db($db);
+
+define('WELCOME_PAGE', 'welcome.php');
+define('DEPTH_SHOW', 5);
+
 
 function __autoload($class_name) {
 	include 'class\\'.$class_name . '.class.php';
