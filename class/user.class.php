05249c29c18480ed0d57edb28bd7799b4ab979ce
Alex <alexander.kuzmin@quotezzz.com>
2012-06-26 10:17:12 +0300

    Fill user class methods with content

--- class/user.class.php
+++ class/user.class.php
@@ -4,9 +4,14 @@ class User
 {
     public static function create($email)
     {
+        $query = 'INSERT INTO user SET email="%s", created_at=NOW()';
+        mysql_query(sprintf($query, $email));
     }
 
     public static function exists($email)
     {
+        $query = 'SELECT 1 FROM user WHERE email = "%s" LIMIT 1';
+        $res = mysql_query(sprintf($query, $email));
+        return mysql_num_rows($res) > 0;
     }
 }
