632252229654c6ab5e43cd63333ca980c5d873fa
Alex <alexander.kuzmin@quotezzz.com>
2012-06-26 09:55:25 +0300

    Remove pseudo code

--- welcome.php
+++ welcome.php
@@ -1,24 +1,18 @@
 <?php
 
 $email = $_POST['email'];
-// if mail was entered and is not valid: display message, stop
 if(!isset($_POST['email'])){
     return;
 }
 
-// if mail was not entered: stop
 if(!Validate::email($email)){
     $message = 'Email is not valid';
     return;
 }
 
-// if mail is new: create user
 if(!User::exists($email)){
     User::create($email);
 }
 
 Auth::login($email);
-// get user for mail
-// log user in
-// redirect to index
 Utility::redirect('index.php');
