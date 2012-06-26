9f8f65896c8c28e3698d18f6cc90e8cf5c2cd8b5
Alex <alexander.kuzmin@quotezzz.com>
2012-06-26 09:48:27 +0300

    Add Weight class

--- class/weight.class.php
+++ class/weight.class.php
@@ -1,1 +1,22 @@
 <?php
+
+class Weight
+{
+    public static function get($date)
+    {
+        $email = Auth::getEmail();
+
+    }
+
+    public static function getPeriod($start, $finish)
+    {
+        $email = Auth::getEmail();
+
+    }
+
+    public static function set($date)
+    {
+        $email = Auth::getEmail();
+
+    }
+}
