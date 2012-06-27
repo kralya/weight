d63eb256eff01be9ba5a5d91ca16b267afc19545
Alex <alexander.kuzmin@quotezzz.com>
2012-06-27 09:38:48 +0300

    Set getForDaysAgo() to return proper results

--- class/weight.class.php
+++ class/weight.class.php
@@ -11,8 +11,21 @@ class Weight
     public static function getForDaysAgo($daysAgo)
     {
         $email = Auth::getEmail();
-        $start = new DateTime('now');
         $finish = new DateTime("-$daysAgo days");
+
+        $query = 'SELECT weight, w.created_at FROM weight w, user u WHERE u.id = w.id_user AND u.email = "%s" AND w.created_at > "%s"';
+        $res = mysql_query(sprintf($query, $email, $finish->format('Y-m-d')));
+
+        if(mysql_num_rows($res) === 0){
+            return array();
+        }
+
+        $result = array();
+        while($row = mysql_fetch_assoc($res)){
+            $result[] = $row;
+        }
+
+        return $result;
     }
 
     public static function set($date)
