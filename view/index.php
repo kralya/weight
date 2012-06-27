3dfc527c4568f1298af488146f850e459e158670
Alex <alexander.kuzmin@quotezzz.com>
2012-06-27 10:38:18 +0300

    Extract .css, .js folder and files. Attach jquery.

--- view/index.php
+++ view/index.php
@@ -1,31 +1,24 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/index.css">
+    <script type="text/javascript" src="js/jquery.min.1.7.1.js"></script>
+</head>
+<body>
 home page here
-<style type="text/css">
-    div.box {
-        /*border: 1px solid #f00;*/
-        width: 50px;
-        height: 20px;
-        display: block;
-        margin-left: 100px;
-    }
-
-    div.container div{
-        float: left;
-    }
-
-    div.container{
-        margin-top: 150px;
-        padding-left: 200px;
-        /*border: 1px dotted black;*/
-        height: 30px;
-    }
-</style>
 
 <div class="container">
     <?php foreach ($weights as $key => $value) { ?>
 
     <div class="box">
-        <?php echo $value?$value:'...' ?>
+        <?php echo $value ? $value : '...' ?>
     </div>
 
     <?php } ?>
 </div>
+</body>
+<script type="text/javascript">
+    $('.box').click(function(){
+//        alert('hello!');
+    });
+</script>
+</html>
