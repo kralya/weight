efabe0426c69332039fc6cc7e072c4b345fc614d
Alex <alexander.kuzmin@quotezzz.com>
2012-06-27 10:04:25 +0300

    Extract orderAndLimit()

--- class/weight.class.php
+++ class/weight.class.php
@@ -16,20 +16,24 @@ class Weight
         $query = 'SELECT weight, w.created_at FROM weight w, user u WHERE u.id = w.id_user AND u.email = "%s" AND w.created_at > "%s"';
         $res = mysql_query(sprintf($query, $email, $finish->format('Y-m-d')));
 
-        if(mysql_num_rows($res) === 0){
+        if (mysql_num_rows($res) === 0) {
             return array();
         }
 
         // get unordered array $x[date] = weight
         $weights = array();
-        while($row = mysql_fetch_assoc($res)){
+        while ($row = mysql_fetch_assoc($res)) {
             $weights[$row['created_at']] = $row['weight'];
         }
 
-        // order it, limit to DAYS_AGO_INDEX values, fill with empty values for absent dates.
+        return self::orderAndLimit($weights, $daysAgo);
+    }
+
+    protected static function orderAndLimit($weights, $limit)
+    {
         $results = array();
-        for($i=0; $i < $daysAgo; $i++){
-            $date = new DateTime(sprintf(' -%s days', ($daysAgo-$i) ));
+        for ($i = 0; $i < $limit; $i++) {
+            $date = new DateTime(sprintf(' -%s days', ($limit - $i)));
             $results[$date->format('Y-m-d')] = isset($weights[$date->format('Y-m-d')]) ? $weights[$date->format('Y-m-d')] : '';
         }
 
