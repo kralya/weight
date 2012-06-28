61e778a202436179cbf92f97c356a4e7640af6cb
Alex <alexander.kuzmin@quotezzz.com>
2012-06-28 09:46:58 +0300

    Prepare and pass all data to AJAX script

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include_once('config.php');
 Auth::redirectUnlogged();
 
 $weights = Weight::getForDaysAgo(DAYS_AGO_INDEX);
+$email = Auth::getEmail();
 
-//var_dump($weights);
 // + write AJAX scripts for data update (not in this file)
 
 Core::loadTemplate('menu');
-Core::loadTemplate('index', array('weights' => $weights));
+Core::loadTemplate('index', array('weights' => $weights, 'email' => $email));
--- view/index.php
+++ view/index.php
@@ -7,9 +7,11 @@
 home page here
 
 <div class="container">
+    <input type="hidden" id="email" value="<?php echo $email ?>" />
+
     <?php foreach ($weights as $key => $value) { ?>
 
-    <div class="box">
+    <div class="box" id="box-<?php echo $key ?>">
         <?php echo $value ? $value : '...' ?>
     </div>
 
@@ -21,24 +23,27 @@ home page here
 <script type="text/javascript">
     function hideBoxes() {
         $('.box').show();
-        $('.active').hide();
+        $('.active').remove();
     }
 
     $('.box').click(function () {
         hideBoxes();
 
         var text = $(this).text();
-        $(this).before('<div class="active"> <input type="text" class="input_box" value="' + $.trim(text) + '"/> <input type="submit" class="input_submit" value="Save"/></div>');
+        $(this).before('<div class="active"> <input type="text" id="input_selected" value="' + $.trim(text) + '"/> <input type="submit" class="input_submit" value="Save"/></div>');
         $(this).hide();
 
-        $('.input_box').click(function () {
+        $('#input_selected').click(function () {
             return false;
         });
 
         $('.input_submit').click(function () {
-            $.post('save.php', {email:1, weight:2, date: 3, sid:Math.random()}, function (response) {
-//                $('#vote_res_' + vid).html(data);
+            var email = $('#email').val();
+            var weight = $('#input_selected').val();
+            var date = $('.box:hidden').attr('id').replace('box-','');
 
+            $.post('save.php', {email: email, weight: weight, date: date, sid:Math.random()}, function (response) {
+//                $('#vote_res_' + vid).html(data);
             });
 
         });
