5e9e11661490b5ce662df7646c0f4fc5155d7846
Alex <alexander.kuzmin@quotezzz.com>
2012-06-28 09:26:22 +0300

    Add sample AJAX script

---
+++ save.php
@@ -0,0 +1,9 @@
+<?php
+
+// 1. validate user email, date, weight. If it is wrong, send 'error' response and exit().
+
+// 2. update / insert values to DB
+
+// 3. send weight received back as success response
+
+echo'ok';
