5f06a3806dec9eb1924f1c687d6c8f568f5f1332
Alex <alexander.kuzmin@quotezzz.com>
2012-06-29 09:47:18 +0300

    Add logo.png Set up a simple header.

--- view/header.php
+++ view/header.php
@@ -1,6 +1,10 @@
-
-<div style="margin-bottom: 20px">
-	<a href='index.php'>View</a>
-	<a href='graph.php'>Graph</a>
-	<a href='logout.php'>Logout</a>
+<div style="width: 100%; border: 1px dotted black; height: 70px">
+    <div style="float: left">
+        <img src="img/logo.png"/>
+    </div>
+    <div style="margin-top: 25px; margin-right: 25px; float: right">
+        <a href='index.php'>View</a>
+        <a href='graph.php'>Graph</a>
+        <a href='logout.php'>Logout</a>
+    </div>
 </div>
