5a5ab30781836a446c9385d3e818ea1b8d924e63
parap <spalchinda@gmail.com>
2012-07-04 09:47:36 +0300

    Fix 'empty user' issue
    For empty user no weights were shown

--- class/weight.class.php
+++ class/weight.class.php
@@ -23,10 +23,6 @@ class Weight
         $query = 'SELECT weight, w.created_at FROM weight w, user u WHERE u.id = w.id_user AND u.email = "%s" AND w.created_at > "%s"';
         $res = mysql_query(sprintf($query, $email, $finish->format('Y-m-d')));
 
-        if (mysql_num_rows($res) === 0) {
-            return array();
-        }
-
         $weights = array();
         while ($row = mysql_fetch_assoc($res)) {
             $weights[$row['created_at']] = $row['weight'];
