074d92316c437db879fe5e265121c4c9543925e4
parap <spalchinda@gmail.com>
2012-08-28 16:41:14 +0300

    Fix JS multiple keydown / click processors

--- js/index-weight.js
+++ js/index-weight.js
@@ -4,10 +4,9 @@ function hideBoxes() {
 
 function sendWeight() {
     var email = $('#email').val();
-
     var boxId = $('.forms:visible').attr('id');
-    var date = boxId.replace('box-', '').replace('-form','');
-    var input = '#' + boxId.replace('-form','') + '-input_selected';
+    var date = boxId.replace('box-', '').replace('-form', '');
+    var input = '#' + boxId.replace('-form', '') + '-input_selected';
     var weight = $(input).val();
 
     $.post('save.php', {email:email, weight:weight, date:date, sid:Math.random()}, function (response) {
@@ -16,11 +15,27 @@ function sendWeight() {
             return false;
         }
 
-        var realBoxId = $('.forms:visible').attr('id').replace('-form','').replace('box-','');
+        var rawBoxId = $('.forms:visible').attr('id');
+        var realBoxId = rawBoxId.replace('-form', '').replace('box-', '');
         $('#text-' + realBoxId).text(response);
         $('.forms').hide();
     });
 }
+
+$('.tx').keydown(function (elem) {
+    if (elem.which == 13) {
+        sendWeight();
+    }
+
+    if (elem.which == 27) {
+        hideBoxes();
+    }
+
+});
+
+$('.input_submit').click(function () {
+    sendWeight();
+});
 
 $('.box').click(function () {
     hideBoxes();
@@ -30,34 +45,15 @@ $('.box').click(function () {
     $('#' + id + '-form').show();
     var input = '#' + id + '-input_selected';
 
-//    $(id+'-form').after('<div class="active"> <input type="text" id="input_selected" value="' + $.trim(text) + '"  onfocus="this.value = this.value;" /> <input type="submit" class="input_submit" value="Сохранить"/></div>');
-//    $(this).hide();
-
     $(input).val($.trim(text));
     $(input).focus();
 
-    if($(input).val() == '...'){
+    if ($(input).val() == '...') {
         $(input).select();
     }
 
     $(input).click(function () {
         return false;
-    });
-
-    $('.input_submit').keydown(function (elem) {
-        if (elem.which == 13) {
-            sendWeight();
-            hideBoxes();
-        }
-
-        if (elem.which == 27) {
-            hideBoxes();
-        }
-
-    });
-
-    $('.input_submit').click(function () {
-        sendWeight();
     });
 
     return false;
