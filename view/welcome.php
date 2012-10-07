803facdb6bd389bf6c29781163e4e3c03a1aa04b
parap <spalchinda@gmail.com>
2012-10-07 17:52:24 +0300

    Add loginza widget to welcome page template

--- view/welcome.php
+++ view/welcome.php
@@ -6,7 +6,16 @@
         <input type="submit" name="submit" class="entr" value="Войти"/>
         <br/>
         <input type="checkbox" class="chec" name="remember"/> <label>Запомнить</label>
+        <br/>
     </form>
+
+    <br/>
+
+    <script src="http://loginza.ru/js/widget.js" type="text/javascript"></script>
+    <a href="http://loginza.ru/api/widget?token_url=http://www.deposit.zp.ua/weight/auth.php" class="loginza">
+        <img src="http://loginza.ru/img/sign_in_button_gray.gif" alt="Войти через loginza"/>
+    </a>
+
 
     <div class="welcome-error"><?php echo $message ?></div>
 </div>
