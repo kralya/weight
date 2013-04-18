a902b23360af30c560b5d6a07899bab37b2f72b5
parap <spalchinda@gmail.com>
2013-04-18 13:38:36 +0300

    Remove sensitive data.

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
 session_start();
 
-$username = 'root';
+$username = 'alex';
 $password = '';
 $db = 'weight';
 $server = 'localhost';
 
-//$username = 'u280846612_root';
-//$password = 'yTKfENMEYM';
-//$db = 'u280846612_weight';
-//$server = 'mysql.hostinger.com.ua';
-
-mysql_connect($server, $username, $password);
+mysql_connect($server, $username, $password) or die('failed to connect');
 mysql_select_db($db);
 
 define('WELCOME_PAGE', 'welcome.php');
