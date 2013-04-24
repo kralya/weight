4f94aaedf906b567a39339e9e81b5c962fd2d6f3
parap <spalchinda@gmail.com>
2013-04-24 09:21:44 +0300

    Rename files to avoid virus infection. Add .htaccess to dabble with index.php

---
+++ av_index.php
@@ -0,0 +1,10 @@
+<?php
+include_once('config.php');
+Auth::redirectUnlogged();
+
+$weights = Weight::getForDaysAgo(DAYS_AGO_INDEX);
+$email = Auth::getEmail();
+
+Core::loadTemplate('av_header', array('title' => 'Дневник веса'));
+Core::loadTemplate('av_index', array('weights' => $weights, 'email' => $email));
+Core::loadTemplate('av_footer', array('link' => 'graph.php', 'linkText' => 'Посмотреть график:'));
