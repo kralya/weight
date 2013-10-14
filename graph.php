38e588af825d726d2cec4b9d27e548cb98c2d499
parap <hrumos@gmail.com>
2013-10-14 23:30:12 +0300

    Set whole graph format to depend from trend format chosen

--- graph.php
+++ graph.php
@@ -1,30 +1,30 @@
 <?php
 include_once('config.php');
 Auth::redirectUnlogged();
-$weights = Weight::getForDaysAgo(10000);
-
 $period = $_GET['trend'];
 
 $days = array('week' => 7, 'month' => 30, 'year' => 365);
-$totalDays = array_key_exists($period, $days) ? $days[$period] : 0;
+$totalDaysZ = array_key_exists($period, $days) ? $days[$period] : 10000;
+$totalDays = array_key_exists($period, $days) ? $days[$period] : 10000;
+$weights = Weight::getForDaysAgo($totalDaysZ);
 
-if($totalDays){
-$points = Weight::getPositiveWeightForDaysAgo($totalDays);
-$trendPoints = Weight::getTrendFor($points);
-}else{
+if ($totalDays) {
+    $points = Weight::getPositiveWeightForDaysAgo($totalDays);
+    $trendPoints = Weight::getTrendFor($points);
+} else {
     $trendPoints = null;
 }
 
 $counter = 0;
-foreach($weights as $key=>$weight){
-    if(!empty($weights[$key]['weight'])){
+foreach ($weights as $key => $weight) {
+    if (!empty($weights[$key]['weight'])) {
         $counter++;
     }
-    if($counter % 2 ==0){
-        $weights[$key]['color'] = ($counter % 4 == 0)? '#CC0000' : '#fd813c';
+    if ($counter % 2 == 0) {
+        $weights[$key]['color'] = ($counter % 4 == 0) ? '#CC0000' : '#fd813c';
     }
 }
 
 Core::loadTemplate('av_header', array('weights' => $weights, 'useChartScript' => true, 'title' => 'График веса', 'trendPoints' => $trendPoints));
-Core::loadTemplate('graph', array('displayWeight' => ($counter > 1) ));
+Core::loadTemplate('graph', array('displayWeight' => ($counter > 1)));
 Core::loadTemplate('av_footer', array('link' => INDEX_PAGE, 'linkText' => 'Ввести вес:'));
