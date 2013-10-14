a0c4ca28a2d2dac6a8c4f3e39a8903e18f7ad477
parap <hrumos@gmail.com>
2013-10-15 00:46:06 +0300

    Add autofocus for input box

--- view/welcome.php
+++ view/welcome.php
@@ -1,8 +1,14 @@
+<script>
+    window.onload = function() {
+        var input = document.getElementById("email").focus();
+    }
+</script>
+
 <div class="wr">
     <h2>Введите адрес электронной почты:</h2>
 
     <form action="welcome.php" method="post" id="my-form">
-        <input type="text" name="email" class="tx" value="<?php echo $email ?>">
+        <input type="text" name="email" class="tx" id="email" value="<?php echo $email ?>" autofocus>
         <input type="submit" name="submit" class="entr" value="Войти"/>
         <br/>
         <input type="checkbox" class="chec" name="remember"/> <label>Запомнить</label>
