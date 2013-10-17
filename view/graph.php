00b12efc9eb55ba712c82d8e1d372f4560af6bf9
parap <hrumos@gmail.com>
2013-10-17 10:25:35 +0300

    Add 'print' button. Add CSS.

--- view/graph.php
+++ view/graph.php
@@ -7,13 +7,28 @@
 <div id="chartdiv" style="width:100%; height:290px;"></div>
 <?php } ?>
 
-<fieldset class="trend">
-    <legend>Тренд:</legend>
-    <ul class="trend">
-        <li>Последний год <input type="checkbox" id="checkbox-year" <?php if($period == 'year') echo "checked" ?> onclick="window.location = '/graph-trend-year';"/></li>
-        <li>Последний месяц <input type="checkbox" id="checkbox-month" <?php if($period == 'month') echo "checked" ?> onclick="window.location = '/graph-trend-month';"/></li>
-        <li>Последняя неделя <input type="checkbox" id="checkbox-week" <?php if($period == 'week') echo "checked" ?> onclick="window.location = '/graph-trend-week';"/></li>
-        <li>Очистить <input type="checkbox" id="checkbox-remove" <?php if($period == '') echo "checked" ?> onclick="window.location = '/graph';"/></li>
-    </ul>
-    <br/>
-</fieldset>
+<div class="bottom-menu">
+    <div class="sub-left">
+        <fieldset class="trend">
+            <legend>Тренд:</legend>
+            <ul class="trend">
+                <li>Последний год <input type="checkbox"
+                                         id="checkbox-year" <?php if ($period == 'year') echo "checked" ?>
+                                         onclick="window.location = '/graph-trend-year';"/></li>
+                <li>Последний месяц <input type="checkbox"
+                                           id="checkbox-month" <?php if ($period == 'month') echo "checked" ?>
+                                           onclick="window.location = '/graph-trend-month';"/></li>
+                <li>Последняя неделя <input type="checkbox"
+                                            id="checkbox-week" <?php if ($period == 'week') echo "checked" ?>
+                                            onclick="window.location = '/graph-trend-week';"/></li>
+                <li>Очистить <input type="checkbox" id="checkbox-remove" <?php if ($period == '') echo "checked" ?>
+                                    onclick="window.location = '/graph';"/></li>
+            </ul>
+        </fieldset>
+    </div>
+
+    <div id="print">
+        <input  type="button" value="Печать" onclick="window.print();">
+    </div>
+
+</div>
