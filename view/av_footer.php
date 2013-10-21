a54d95239f3db3bb59c2251007b6e50659df27b0
parap <hrumos@gmail.com>
2013-10-21 14:35:07 +0300

    Add link to LJ to allow users post comments, ideas and reviews.

--- view/av_footer.php
+++ view/av_footer.php
@@ -4,7 +4,8 @@
         <?php if (isset($linkText)) { ?>
         <div class="bg-bt"><a class="view-t-schedule" href="<?php echo $link ?>"><?php echo $linkText ?></a></div>
         <?php } ?>
-        <span class="copy">&copy; Быстрый график <?php echo date('Y') ?></span>
+<!--        <span class="copy">&copy; Быстрый график --><?php //echo date('Y') ?><!--</span>-->
+        <span class="copy"><a href="http://graph-of-weight.livejournal.com/" target="_blank">Оставить отзыв:</a></span>
     </div>
 </div>
 <!--    footer end-->
