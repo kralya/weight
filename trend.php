bde0663cd6682c092ed1802a5761e84501641f36
parap <hrumos@gmail.com>
2013-10-22 10:15:03 +0300

    Split graph and trend controllers. Add new routes for special graphs (not working still). Add display errors values in .htaccess. It is commented out for prod server.

--- trend.php
+++ trend.php
@@ -1,13 +1,27 @@
 <?php
 include_once('config.php');
 Auth::redirectUnlogged();
+$period = $_GET['trend'];
 
-$weights    = Weight::getForDaysAgo(10000);
+$days       = array('week' => 7, 'month' => 30, 'year' => 365);
+$totalDaysZ = array_key_exists($period, $days) ? $days[$period] : 10000;
+$totalDays  = array_key_exists($period, $days) ? $days[$period] : 0;
+$weights    = Weight::getForDaysAgo($totalDaysZ);
+
+if ($totalDays) {
+    $points      = Weight::getPositiveWeightForDaysAgo($totalDays);
+    $trendPoints = Weight::getTrendFor($points, $totalDays);
+} else {
+    $trendPoints = null;
+}
 
 $counter = 0;
 foreach ($weights as $key => $weight) {
     if (!empty($weights[$key]['weight'])) {
         $counter++;
+    }
+    if ($counter % 2 == 0) {
+        $weights[$key]['color'] = ($counter % 4 == 0) ? '#CC0000' : '#fd813c';
     }
 }
 
@@ -21,9 +35,9 @@ if (count($weight) > 100) {
 Core::loadTemplate('av_header', array('weights'        => $weights,
                                       'useChartScript' => true,
                                       'title'          => 'График веса',
-                                      'trendPoints'    => array(),
+                                      'trendPoints'    => $trendPoints,
                                       'bulletSize'     => $bulletSize));
 Core::loadTemplate('graph', array('displayWeight' => ($counter > 1),
-                                  'period'        => 0));
+                                  'period'        => $period));
 Core::loadTemplate('av_footer', array('link'     => INDEX_PAGE,
                                       'linkText' => 'Ввести вес:'));
