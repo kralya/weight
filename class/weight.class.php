72378bca244a84561580308be33fd49221525ff1
parap <hrumos@gmail.com>
2013-10-24 13:26:14 +0300

    Build inline $email variable

--- class/weight.class.php
+++ class/weight.class.php
@@ -155,16 +155,14 @@ class Weight
             $finish = new DateTime("-$daysAgo days");
             $start  = new DateTime("-$offset days");
         } else {
-            $finish = new DateTime("-$daysAgo days");
-            $start  = new DateTime("-$offset days");
+            $finish = new DateTime($daysAgo);
+            $start  = new DateTime($offset);
         }
-
-        $email = Auth::getEmail();
 
         $query = 'SELECT weight, w.created_at FROM weight w, user u
         WHERE u.id = w.id_user AND u.email = "%s" AND w.created_at >= "%s" AND w.created_at <= "%s" AND w.weight <> ""';
 
-        $res = mysql_query(sprintf($query, $email, $finish->format('Y-m-d'), $start->format('Y-m-d')));
+        $res = mysql_query(sprintf($query, Auth::getEmail(), $finish->format('Y-m-d'), $start->format('Y-m-d')));
 
         $weights = array();
         while ($row = mysql_fetch_assoc($res)) {
