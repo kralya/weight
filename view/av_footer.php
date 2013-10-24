c992f9aa996a55c98dbc14c5e590f970393e3f4c
parap <hrumos@gmail.com>
2013-10-24 14:26:54 +0300

    Update bottom contact form link to lead to comment, not to general post.

--- view/av_footer.php
+++ view/av_footer.php
@@ -4,8 +4,7 @@
         <?php if (isset($linkText)) { ?>
         <div class="bg-bt"><a class="view-t-schedule" href="<?php echo $link ?>"><?php echo $linkText ?></a></div>
         <?php } ?>
-<!--        <span class="copy">&copy; Быстрый график --><?php //echo date('Y') ?><!--</span>-->
-        <span class="copy"><a href="http://graph-of-weight.livejournal.com/" target="_blank">Оставить отзыв:</a></span>
+        <span class="copy"><a href="http://graph-of-weight.livejournal.com/570.html?mode=reply#add_comment" target="_blank">Оставить отзыв:</a></span>
     </div>
 </div>
 <!--    footer end-->
