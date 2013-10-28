c5275c3945132b6e8231b1d13214dedd247daa01
parap <hrumos@gmail.com>
2013-10-28 23:49:45 +0200

    Add sample of routing use.

--- config.php
+++ config.php
@@ -19,3 +19,28 @@ date_default_timezone_set('Europe/Moscow');
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->add(false, __DIR__ . '/class');
 $loader->register();
+
+
+// routes
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+$route = new Route('/graph', array('controller' => 'GraphController'));
+$routes = new RouteCollection();
+$routes->add('route_name', $route);
+
+$context = new RequestContext($_SERVER['REQUEST_URI']);
+$matcher = new UrlMatcher($routes, $context);
+
+$path = $_SERVER['REQUEST_URI'];
+
+try{
+    $parameters = $matcher->match($path);
+    $controller = $parameters['controller'];
+} catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e){
+    $controller = 'inputController';
+}
+
+//var_dump($controller);
