967ef84916061ec4dee94fac054c71d897a7910f
parap <hrumos@gmail.com>
2013-11-11 14:04:23 +0200

    Move php files from root folder to /controller folder.
    It is to later switch them to classes.

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ $routes = new RouteCollection();
 $routes->add('index', new Route('/', array('controller' => 'IndexController')));
 $routes->add('graph', new Route('/graph', array('controller' => 'GraphController')));
 $routes->add('graph_term', new Route('/graph-for-{graph}/{term}', array('controller' => 'GraphController')));
-
-$routes->add('graph_trend', new Route('/graph-trend-{graph}/{term}', array('controller' => 'TrendController')));
+$routes->add('graph_trend', new Route('/graph-trend-{trend}/{term}', array('controller' => 'TrendController')));
+$routes->add('graph_trend', new Route('/graph-trend-{trend}', array('controller' => 'TrendController')));
 $routes->add('welcome', new Route('/welcome', array('controller' => 'WelcomeController')));
 $routes->add('logout', new Route('/logout', array('controller' => 'LogoutController')));
 $routes->add('save', new Route('/save', array('controller' => 'SaveController')));
@@ -27,11 +27,14 @@ $path = $_SERVER['REQUEST_URI'];
 
 try {
     $parameters = $matcher->match($path);
+
     $controller = $parameters['controller'];
 } catch (ResourceNotFoundException $e) {
     $controller = 'indexController';
 }
 $file      = strtolower(str_replace('Controller', '', $controller)).'.php';
 $inclusion = file_exists('controller/' . $file) ? 'controller/' . $file : 'controller/index.php';
+
+//var_dump($inclusion);
 
 include_once($inclusion);
