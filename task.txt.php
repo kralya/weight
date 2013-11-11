5e73d8f704a1aae182947d95501e1c3a481745d4
parap <hrumos@gmail.com>
2013-11-11 15:13:27 +0200

    Update task file. Set Routing task done, add follow-up tasks.

--- task.txt.php
+++ task.txt.php
@@ -1,32 +1,25 @@
 <?php
 /**
-Dev: при наведении мышки на квадрат давать его увеличенное изображение?
-[16:50:32] client: но в квадрат круто пместить хотя бы без даты просто день недели
-[16:50:33] client: будет смотреться
-[16:50:40] client: слева
-[16:50:54] client: тогда взгляд захватывает всю инфу
-[16:50:59] client: и при увеличении квадрата тоже будет
-
- **/
-
-/**
  *
  * DONE - increase size of bullets
  * DONE - Move week number in dropdown by -1.
- *
  * DONE - Set weeks numbers in dropdown to be displayed only if the week has 2 or more entries
- * IMPOSSIBLE - Add increased image of weight input box;
- *
- * - Set possibility to build trend on any date interval.
- * - Add routing: try symfony routing component to avoid wheel re-inventing.
- * - Add possibility to add link, that is active for some time, and displays data for this user for given graph.
- * - add index of body mass, m / (h*h), h-height
- * - possibility to set weight for older periods
- * - fix loginza
+ * IMPOSSIBLE - Add increased image of weight input box; WHY: it is not image, but complex HTML div element.
+ * DONE Add routing: try symfony routing component to avoid wheel re-inventing.
  *
  * - An obvious call to action
  * - Customer testimonials
  * - Easy-to-find contact information
  * - A clear description of who you are
+
+ *
+ * - Set possibility to build trend on any date interval.
+ * - Add possibility to add link, that is active for some time, and displays data for this user for given graph.
+ * - add index of body mass, m / (h*h), h-height
+ * - possibility to set weight for older periods
+ * - fix loginza
+ * - Add file, containing routes.
+ * - Use twig.
+ *
  **/
 ?>
